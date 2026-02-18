7fbabc6a9dede29311b1df85d736e98eac2d39ca
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-02-18 23:02:21 +0530

    [Paarth Agarwal] Add. Implement the UC2 Inches class and corresponding unit tests for equality checks

--- QuantityMeasurementApp/src/main/java/org/example/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/main/java/org/example/QuantityMeasurementApp.java
@@ -1,6 +1,7 @@
 package org.example;
 
 public class QuantityMeasurementApp {
+    //Inner class to represent Feet measurement
     public static class Feet{
         private final double value;
         public Feet(double value){
@@ -26,10 +27,49 @@ public class QuantityMeasurementApp {
         }
     }
 
+    //Inner class to represent Feet measurement
+    public static class Inches{
+        private final double value;
+        public Inches(double value){
+            this.value=value;
+        }
+        public double getValue(){
+            return value;
+        }
+        @Override
+        public boolean equals(Object obj){
+            if(this==obj){
+                return true;
+            }
+            if(obj==null||getClass()!=obj.getClass()){
+                return false;
+            }
+            Inches other=(Inches) obj;
+            return Double.compare(this.value,other.value)==0;
+        }
+        @Override
+        public int hashCode(){
+            return Double.hashCode(value);
+        }
+    }
+
+    //Define a static method to demonstrate Feet eqaulity check
+    public static boolean demonstrateFeetEquality(double value1, double value2) {
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+        return f1.equals(f2);
+    }
+    //Define a static method to demonstrate Inches eqaulity check
+    public static boolean demonstrateInchesEquality(double value1, double value2) {
+        Inches i1 = new Inches(value1);
+        Inches i2 = new Inches(value2);
+        return i1.equals(i2);
+    }
+
     public static void main(String[] args) {
-        Feet f1=new Feet(1.0);
-        Feet f2=new Feet(1.0);
-        System.out.println(f1.equals(f2));
+
+        System.out.println("Feet Equal: " + demonstrateFeetEquality(1.0, 1.0));
+        System.out.println("Inches Equal: " + demonstrateInchesEquality(1.0, 1.0));
     }
 
 }
